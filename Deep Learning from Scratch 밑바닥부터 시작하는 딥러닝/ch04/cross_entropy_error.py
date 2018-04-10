70c5ea6a75b58825692bf4515e00094aa6a3e53f
ghdic <ghdic@naver.com>
2018-04-10 11:09:04 +0900

    finish

---
+++ Deep Learning from Scratch 밑바닥부터 시작하는 딥러닝/ch04/cross_entropy_error.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+
+def mean_squared_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta))
+
+t = [0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+y = [0.1, 0.05, 0.6, 0.0, 0.05, 0.1, 0.0, 0.1, 0.0, 0.0]
+result = mean_squared_error(np.array(y), np.array(t))
+print(result)
+
+y = [0.1, 0.05, 0.1, 0.0, 0.05, 0.1, 0.0, 0.6, 0.0, 0.0]
+result = mean_squared_error(np.array(y), np.array(t))
+print(result)
